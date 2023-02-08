9fdb75b0d0adfc7f5242f242f369bc13847de6d8
mageunwon <geunwon1947@gmail.com>
2023-02-08 15:05:44 +0900

    리액트 처음부터 다시 공부중! jsx 문법 규칙과 css, class적용 연습 다음은 props와 state공부 할 것!

---
+++ hello-react/src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+
+const App = () =>{
+	
+  //css 직접 지정
+	// const style = {
+	// 	backgroundColor: 'black',
+	// 	padding: '16px',
+	// 	color: 'white',
+	// 	fontSize: '12px'
+	// };
+
+  // return (
+  //   <div style = {style}>
+	// 		안녕??!
+	// 	</div>
+  // )
+  
+  //클래스 적용 방식
+  return (
+    <div className="App">
+      안녕??!
+    </div>
+  )
+}
+
+export default App;
